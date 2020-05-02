64bbca0c07f90968d30185acc78d42c475ae40c0
Batman <57849598+eat-sleep-code-repeat-it@users.noreply.github.com>
2020-05-02 11:07:12 -0400

    fix swagger issue when publish to iis subfolder

--- NetCoreWebApi/Controllers/BookChaptersController.cs
+++ NetCoreWebApi/Controllers/BookChaptersController.cs
@@ -8,21 +8,38 @@ using NetCoreWebApi.Models;
 
 namespace NetCoreWebApi.Controllers
 {
+    /// <summary>
+    /// BookChaptersController
+    /// </summary>
     [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class BookChaptersController : ControllerBase
     {
         private readonly IBookChaptersRepository _repository;
+
+        /// <summary>
+        /// BookChaptersController
+        /// </summary>
+        /// <param name="repository"></param>
         public BookChaptersController(IBookChaptersRepository repository)
         {
             _repository = repository;
         }
 
+        /// <summary>
+        /// GetBookChaptersAsync
+        /// </summary>
+        /// <returns></returns>
         // GET: api/bookchapters
         [HttpGet()]
         public Task<IEnumerable<BookChapter>> GetBookChaptersAsync() => _repository.GetAllAsync();
 
+        /// <summary>
+        /// GetBookChapterByIdAsync
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         // GET api/bookchapters/guid
         [HttpGet("{id}", Name = nameof(GetBookChapterByIdAsync))]
         public async Task<IActionResult> GetBookChapterByIdAsync(Guid id)
@@ -38,6 +55,11 @@ namespace NetCoreWebApi.Controllers
             }
         }
 
+        /// <summary>
+        /// PostBookChapterAsync
+        /// </summary>
+        /// <param name="chapter"></param>
+        /// <returns></returns>
         // POST api/bookchapters
         [HttpPost]
         public async Task<IActionResult> PostBookChapterAsync([FromBody]BookChapter chapter)
@@ -51,6 +73,12 @@ namespace NetCoreWebApi.Controllers
             return CreatedAtRoute(nameof(GetBookChapterByIdAsync), new { id = chapter.Id }, chapter);
         }
 
+        /// <summary>
+        /// PutBookChapterAsync
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="chapter"></param>
+        /// <returns></returns>
         // PUT api/bookchapters/guid
         [HttpPut("{id}")]
         public async Task<IActionResult> PutBookChapterAsync(Guid id, [FromBody]BookChapter chapter)
@@ -68,6 +96,11 @@ namespace NetCoreWebApi.Controllers
             return new NoContentResult();  // 204
         }
 
+        /// <summary>
+        /// DeleteAsync
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         // DELETE api/bookchapters/guid
         [HttpDelete("{id}")]
         public async Task DeleteAsync(Guid id)
--- NetCoreWebApi/Startup.cs
+++ NetCoreWebApi/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -40,21 +42,29 @@ namespace NetCoreWebApi
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookService", Version = "v1" });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseDeveloperExceptionPage();
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "BookService API V1");
+                // the following line casue issue when you deploy api under IIS in sub application
+                //c.RoutePrefix = string.Empty;
             });
+            
 
             if (env.IsDevelopment())
             {
