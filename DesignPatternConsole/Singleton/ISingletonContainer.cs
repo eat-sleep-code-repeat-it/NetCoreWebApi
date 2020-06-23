9fa2b0a5409fd73630b3f825c34da4efc90f7ccb
Batman <57849598+eat-sleep-code-repeat-it@users.noreply.github.com>
2020-06-22 22:50:10 -0400

    singleton parttern

---
+++ DesignPatternConsole/Singleton/ISingletonContainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternConsole.Singleton
+{
+    public interface ISingletonContainer
+    {
+        int GetPopulation(string name);
+    }
+}
